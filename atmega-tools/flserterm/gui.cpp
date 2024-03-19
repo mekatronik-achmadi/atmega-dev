fbe3cd97b8577230a21a5c2848e28c0dff88f825
mekatronik-achmadi <mekatronik.achmadi@gmail.com>
2024-03-19 08:45:12 +0700

    FLTK Serial still failed

--- atmega-tools/flserterm/gui.cpp
+++ atmega-tools/flserterm/gui.cpp
@@ -8,66 +8,128 @@ static void onBtnSend(Fl_Widget *w, void *p){
     std::cout <<  ((Fl_Input*)p)->value() << std::endl;
 }
 
+static void onBtnClear(Fl_Widget *w, void *p){
+    ((Gui*)p)->bufOutput->text("");
+}
+
+static void onBtnQuit(Fl_Widget *w, void *p){
+    ((Gui*)p)->close();
+}
+
+static void onSerialTimer(void *p){
+    char ch; bool r;
+
+    do {
+        ch = ((Gui*)p)->comPort.ReadChar(r);
+        if(r) ((Gui*)p)->serialChar(ch);
+    } while(r);
+
+    Fl::repeat_timeout(0.01, onSerialTimer,p);
+}
+
+
 Gui::Gui() {
     mainWnd = new Fl_Window(380,500, "FLTK Serial");
+
     btnOpen = new Fl_Button(5,10,85,20,"Open");
     btnSend = new Fl_Button(95,10,85,20,"Send");
-    txtBaud = new Fl_Input(5,35,175,20);
+    btnClr  = new Fl_Button(5,35,85,20, "Clear");
+    btnQuit = new Fl_Button(95,35,85,20, "Quit");
+
     txtSend = new Fl_Input(185,10,190,20);
-    txtPort = new Fl_Input(185,35,190,20);
+    txtPort = new Fl_Input(185,35,95,20);
+    cmbBaud = new Fl_Choice(285,35,90,20);
+
     txtOutput = new Fl_Text_Display(5, 65,370,400);
     statusBar = new Fl_Box(5,475,370,20);
 
-    txtBaud->insert("9600",0);
-    txtPort->insert("/dev/ttyS0");
-
     bufOutput = new Fl_Text_Buffer();
     txtOutput->buffer(bufOutput);
-    bufOutput->text("Received Data");
+    bufOutput->text("");
 
     statusBar->align(FL_ALIGN_LEFT|FL_ALIGN_INSIDE);
     status("Ready");
 
     btnSend->callback(onBtnSend,txtSend);
     btnOpen->callback(onBtnOpen,this);
+    btnClr->callback(onBtnClear,this);
+    btnQuit->callback(onBtnQuit,this);
 
     sttOpen = false;
+
+    cmbBaud->add("9600");
+    cmbBaud->add("38400");
+    cmbBaud->add("57600");
+    cmbBaud->add("74880");
+    cmbBaud->add("115200");
+    cmbBaud->value(0);
+
+    txtPort->insert("/dev/ttyUSB0");
+
 #ifdef ceWINDOWS
-	comPort = new ceSerial("\\\\.\\COM1",9600,8,'N',1); // Windows
+    comPort.SetPort("\\\\.\\COM1");
 #else
-	comPort = new ceSerial("/dev/ttyS0",9600,8,'N',1); // Linux
+	comPort.SetPort("/dev/ttyUSB0");
 #endif
 
-    Fl::scheme("none");
+    Fl::add_timeout(0.01, onSerialTimer,this);
+
+    Fl::scheme("gtk+");
     mainWnd->end();
 }
 
 void Gui::portOpen(){
     if(sttOpen){
-        if(comPort->IsOpened()) {
-            comPort->Close();
+        if(comPort.IsOpened()) {
+            comPort.Close();
             btnOpen->label("Open");
             sttOpen = false;
 
-            status("Close Port: " + std::string(txtPort->value()));
         }
     }
     else {
-        if(comPort->IsOpened()) return;
+        if(comPort.IsOpened()) return;
+        if(std::string(txtPort->value()).length()<3) return;
 
-        comPort->SetPort(std::string(txtPort->value()));
-        comPort->SetBaudRate(std::stoul(txtBaud->value()));
+        long vBaud = 9600;
+        switch(cmbBaud->value()){
+            case 0: vBaud = 9600; break;
+            case 1: vBaud = 38400; break;
+            case 2: vBaud = 57600; break;
+            case 3: vBaud = 74880; break;
+            case 4: vBaud = 115200; break;
+        }
 
-        if(comPort->Open()){
+        comPort.SetBaudRate(vBaud);
+        comPort.SetPort(std::string(txtPort->value()));
+
+        if(comPort.Open()){
             btnOpen->label("Close");
             sttOpen = true;
-            status("Open Port: " + std::string(txtPort->value()));
+
+            status("Open Port: " +
+                    std::string(txtPort->value()) +
+                    "-" +
+                    std::to_string(vBaud));
         }
         else {
             btnOpen->label("Open");
             sttOpen = false;
-            status("Open Failed: " + std::string(txtPort->value()));
+
+            status("Failed Port: " +
+                    std::string(txtPort->value()) +
+                    "-" +
+                    std::to_string(vBaud));
+
         }
+    }
+}
+
+void Gui::serialChar(char ch){
+    if(ch=='\n'){}
+    else{
+        if(isprint(ch) || isspace(ch))
+            bufOutput->append(&ch);
     }
 }
 
@@ -81,6 +143,7 @@ void Gui::show(int argc, char* argv[]){
 }
 
 void Gui::close(){
+    if(comPort.IsOpened()) comPort.Close();
     mainWnd->hide();
 }
 
--- atmega-tools/flserterm/gui.h
+++ atmega-tools/flserterm/gui.h
@@ -4,9 +4,12 @@
 #include <iostream>
 
 #include <FL/Fl.H>
+#include <FL/fl_message.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Button.H>
 #include <FL/Fl_Input.H>
+#include <FL/Fl_Choice.H>
+#include <FL/Fl_Menu_Item.H>
 #include <FL/Fl_Box.H>
 #include <FL/Fl_Text_Display.H>
 #include <FL/Fl_Text_Buffer.H>
@@ -19,25 +22,31 @@ public:
     Gui ();
 
     Fl_Window *mainWnd;
+
     Fl_Button *btnOpen;
     Fl_Button *btnSend;
+    Fl_Button *btnClr;
+    Fl_Button *btnQuit;
 
-    Fl_Input *txtBaud;
     Fl_Input *txtSend;
     Fl_Input *txtPort;
+    Fl_Choice *cmbBaud;
 
     Fl_Text_Display *txtOutput;
     Fl_Text_Buffer *bufOutput;
 
     Fl_Box *statusBar;
 
-    ceSerial *comPort;
+    // comport can't be as an object
+    ceSerial comPort;
     bool sttOpen;
 
     void show(int argc, char* argv[]);
     void close(void);
     void status(std::string msg);
+
     void portOpen(void);
+    void serialChar(char ch);
 };
 
 #endif
